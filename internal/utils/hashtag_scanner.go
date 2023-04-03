aa5b1a8dd98a9f9b7b19fab07fdd78212ba6c73f
Özcan Kasal <ozcankasal@gmail.com>
2023-04-03 14:46:11 +0300

    Hashtag scanner tag extraction functions added

--- internal/utils/hashtag_scanner.go
+++ internal/utils/hashtag_scanner.go
@@ -1,6 +1,13 @@
 package utils
 
-import "github.com/ozcankasal/zettelo/internal"
+import (
+	"bufio"
+	"bytes"
+	"regexp"
+	"strings"
+
+	"github.com/ozcankasal/zettelo/internal"
+)
 
 /*
 MapTagToCanonicalType maps a tag to its canonical type.
@@ -27,4 +34,120 @@ func MapTagToCanonicalType(tag string, config internal.Config) string {
 	}
 
 	return ""
+}
+
+/*
+removeHashtagsFromLine removes hashtags from a line.
+
+Usage:
+
+	line := "This is a #line with #hashtags"
+	lineWithoutHashtags := removeHashtagsFromLine(line)
+
+Parameters:
+
+	line (string): the line to remove hashtags from
+
+Returns:
+
+	(string): the line without hashtags
+*/
+func RemoveHashtagsFromLine(line string) string {
+	return regexp.MustCompile(`(?:^|\s)(#[^\s]+)`).ReplaceAllString(line, "")
+}
+
+func extractTagsFromLine(line string) []string {
+	re := regexp.MustCompile(`(?:^|\s)(#[^\s#][^\s]*)`)
+	return re.FindAllString(line, -1)
+}
+
+/*
+ExtractTaggedLines extracts tagged lines from a file.
+
+Usage:
+
+	fileName := "test.md"
+	data := []byte("#tag1: value1	#tag2: value2")
+	taggedLines := ExtractTaggedLines(fileName, data, config)
+
+Parameters:
+
+	fileName (string): the name of the file
+	data ([]byte): the file contents
+	config (internal.Config): the configuration to use
+
+Returns:
+
+	([]internal.TaggedLine): the tagged lines in the file contents with their values and file paths (if any)
+*/
+func ExtractTaggedLines(fileName string, data []byte, config internal.Config) []internal.TaggedLine {
+	var result []internal.TaggedLine
+	var currentTag string
+
+	reader := bytes.NewReader(data)
+	scanner := bufio.NewScanner(reader)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		tags := extractTagsFromLine(line)
+
+		if len(tags) > 0 {
+			for _, t := range tags {
+				tag := strings.TrimSpace(t)
+				value := strings.TrimSpace(strings.Replace(line, t, "", -1))
+				value = strings.TrimSpace(RemoveHashtagsFromLine(value))
+
+				// Map the tag to its canonical type
+				canonicalType := MapTagToCanonicalType(tag, config)
+				if canonicalType == "" {
+					canonicalType = tag
+				}
+
+				if len(value) > 0 {
+					// This line has a value
+					found := false
+					for i := range result {
+						if result[i].Tag == canonicalType {
+							result[i].Tag = canonicalType
+							canonicalValue := internal.ResultValue{Line: value, FilePath: fileName}
+							result[i].Values = append(result[i].Values, canonicalValue)
+							found = true
+							break
+						}
+					}
+					if !found {
+						// This is the first line with this tag
+						currentValue := internal.ResultValue{Line: value, FilePath: fileName}
+						result = append(result, internal.TaggedLine{Tag: canonicalType, Values: []internal.ResultValue{currentValue}})
+
+					}
+				} else {
+					// This line is a tag with an empty value
+					found := false
+					for i := range result {
+						if result[i].Tag == canonicalType {
+							found = true
+							break
+						}
+					}
+					if !found {
+						// This is the first line with this tag
+						result = append(result, internal.TaggedLine{Tag: canonicalType, Values: []internal.ResultValue{}})
+					}
+				}
+
+				currentTag = canonicalType
+			}
+		} else {
+			// This line is not a tag line, ignore it
+			if len(result) > 0 && currentTag == result[len(result)-1].Tag {
+				// This line is a continuation of the previous line
+			} else {
+				currentTag = ""
+			}
+		}
+	}
+
+	return result
 }
--- internal/utils/hashtag_scanner_test.go
+++ internal/utils/hashtag_scanner_test.go
@@ -1,6 +1,7 @@
 package utils_test
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/ozcankasal/zettelo/internal"
@@ -53,5 +54,66 @@ func TestMapTagToCanonicalType(t *testing.T) {
 		if actual != testCase.expected {
 			t.Errorf("Expected %s, got %s", testCase.expected, actual)
 		}
+	}
+}
+
+func TestRemoveHashtagsFromLine(t *testing.T) {
+	// Define test cases
+	testCases := []struct {
+		line     string
+		expected string
+	}{
+		{
+			line:     "This is a #line with #hashtags",
+			expected: "This is a with",
+		},
+		{
+			line:     "This is a #line with #hashtags and #more hashtags",
+			expected: "This is a with and hashtags",
+		},
+		{
+			line:     "This is a line with no hashtags",
+			expected: "This is a line with no hashtags",
+		},
+	}
+
+	// Run test cases
+	for _, testCase := range testCases {
+		actual := utils.RemoveHashtagsFromLine(testCase.line)
+		if actual != testCase.expected {
+			t.Errorf("Expected %s, got %s", testCase.expected, actual)
+		}
+	}
+}
+
+func TestExtractTaggedLines(t *testing.T) {
+	// Define test data
+	fileName := "test.txt"
+	data := []byte("#tag1 value1\n#tag2 value2\nline 1\n#tag1 value3\n")
+	config := internal.Config{TagMappings: map[string]string{"#tag1": "#canonicalTag1", "#tag2": "#canonicalTag2"}}
+
+	// Expected output
+	expected := []internal.TaggedLine{
+		{
+			Tag: "#canonicalTag1",
+			Values: []internal.ResultValue{
+				{FilePath: fileName, Line: "value1"},
+				{FilePath: fileName, Line: "value3"},
+			},
+		},
+		{
+			Tag: "#canonicalTag2",
+			Values: []internal.ResultValue{
+				{FilePath: fileName, Line: "value2"},
+			},
+		},
+	}
+
+	// Call the function
+	result := utils.ExtractTaggedLines(fileName, data, config)
+
+	// Check the result
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("ExtractTaggedLines() returned unexpected result:\nExpected: %v\nGot: %v", expected, result)
 	}
 }
